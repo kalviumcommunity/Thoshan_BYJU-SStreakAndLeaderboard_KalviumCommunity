44b03e8d4c4f8a780d91368a94465d1bac8aabff
madhvendrasjhalas112-stack <madhvendras.jhala.s.112@kalvium.community>
2026-08-18 17:28:24 +0530

    Last week's work

--- Thoshan_BYJU-SStreakAndLeaderboard_KalviumCommunity/byjus-streaks-frontend/src/App.tsx
+++ Thoshan_BYJU-SStreakAndLeaderboard_KalviumCommunity/byjus-streaks-frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Leaderboard from './pages/Leaderboard';
-
 function App() {
   return (
     <Router>
